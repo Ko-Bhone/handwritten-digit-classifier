a2c008600e1532dbbc0f76a3ed7be57aaf1eb55b
Ko Bhone <bhonem908@gmail.com>
2026-07-17 12:50:37 +0630

    feat: add evaluation metrics

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score)
 
 class ModelEvaluator:
 
@@ -12,3 +13,17 @@ class ModelEvaluator:
             total = y_test.size(0)
             accuracy = (correct / total) * 100
             return accuracy
+
+    def calculate_metrics(self,model,x_test,y_test):
+        model.eval()
+        with torch.no_grad():
+            output = model(x_test)
+            predictions = torch.argmax(output,dim=1)
+        y_true = y_test.numpy()
+        y_pred = predictions.numpy()
+        metric = {"accuracy":accuracy_score(y_true,y_pred),
+                  "precision":precision_score(y_true,y_pred,average="macro"),
+                  "recall":recall_score(y_true,y_pred,average="macro"),
+                  "f1_score":f1_score(y_true,y_pred,average="macro")}
+
+        return metric
