23ca6e17120b244c4eb9e5269caabe8a2c050eb4
Ko Bhone <bhonem908@gmail.com>
2026-07-18 15:03:08 +0630

    imporve processing readablility with static method and formatting

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,11 +2,12 @@ from sklearn.model_selection import train_test_split
 
 
 class DataPreprocessor:
+    @staticmethod
     def split_data(self, x, y, train_size=0.70, validation_size=0.15, test_size=0.15, random_state=42):
         x_train, x_temp, y_train, y_temp = train_test_split(x, y,
-            test_size=(1 - train_size),
-            random_state=random_state,
-            stratify=y)
+                                                test_size=(1 - train_size),
+                                                random_state=random_state,
+                                                stratify=y)
 
         validation_ratio = validation_size / (validation_size + test_size)
         x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, train_size=validation_ratio, random_state=random_state, stratify=y_temp)
